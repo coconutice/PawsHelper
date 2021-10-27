b1e9a7601bb600645be88374689df423bac9f8a8
zhangzhe <zhangz87@uw.edu>
2021-10-27 18:21:45 -0400

    clean and sort with intake and outcome data

--- src/clean_data_v1.py
+++ src/clean_data_v1.py
@@ -5,7 +5,6 @@ import numpy as np
 df_intakes = pd.read_csv("../data/Austin_Animal_Center_Intakes.csv")
 df_outcomes = pd.read_csv("../data/Austin_Animal_Center_Outcomes.csv")
 
-
 # Some repetitive `Animal ID`, what do we do?
 # Step1:
 # Vertically stack intakes and outcomes (130k rows + 130k rows = 265k rows)
@@ -27,8 +26,8 @@ df_join['action_type'] = "nan"
 df_join['action_type'][mask] = "outcome"
 df_join['action_type'][~mask] = "intake"
 df_join = df_join.reset_index(drop=True)
-df_join['DateTime'] = pd.to_datetime(df_join['DateTime']) # make sure DateTime is datetime
-df_join_sorted = df_join.sort_values(by = ['Animal ID', 'DateTime'])
+df_join['DateTime'] = pd.to_datetime(df_join['DateTime'])  # make sure DateTime is datetime
+df_join_sorted = df_join.sort_values(by=['Animal ID', 'DateTime'])
 
 # merge two "neighboring" rows
 
@@ -38,37 +37,47 @@ prev = ""
 prevId = ""
 data_in_out = []
 data_row = []
-incolumns = ['Animal ID', 'Name', 'DateTime', 'MonthYear', 'Found Location',
-        'Intake Type', 'Intake Condition', 'Animal Type', 'Sex upon Intake',
-        'Age upon Intake', 'Breed', 'Color']
+incolumns = ['Animal ID', 'Name', 'DateTime', 'Found Location',
+             'Intake Type', 'Intake Condition', 'Animal Type', 'Sex upon Intake',
+             'Age upon Intake', 'Breed', 'Color']
+
 outcolumns = ['DateTime', 'Date of Birth', 'Outcome Type',
-       'Outcome Subtype', 'Sex upon Outcome', 'Age upon Outcome']
+              'Outcome Subtype', 'Sex upon Outcome', 'Age upon Outcome']
 for idx, row in df_join_sorted.iterrows():
-  t, tid = row['action_type'], row['Animal ID']
-  if tid == prevId and t == "outcome" and prev == "intake":
-      # combine and append
-      data_row.extend(row[outcolumns].values.tolist())
-      data_in_out.append(data_row)
-      data_row = []
-  else:
-      # append if exist otherwise new datarow
-      if len(data_row) != 0:
-        if prev == "intake":
-            # not complete, although outculmns are nan, extend them
-            data_row.extend(df_join_sorted.iloc[idx-1][outcolumns].values.tolist())
-        else: pass
+    t, tid = row['action_type'], row['Animal ID']
+    if tid == prevId and t == "outcome" and prev == "intake":
+        # combine and append
+        data_row.extend(row[outcolumns].values.tolist())
         data_in_out.append(data_row)
-      # anyways, we need incolumns
-      data_row = row[incolumns].values.tolist()
-  prev, prevId = t, tid
+        data_row = []
+    else:
+        if len(data_row) != 0:
+            if prev == "intake":
+                tmp = df_join_sorted.iloc[idx - 1][outcolumns].values.tolist()
+                tmp[0] = np.nan
+                data_row.extend(tmp)
+            data_in_out.append(data_row)
+            data_row = []
+
+        if t == "intake":
+            data_row = row[incolumns].values.tolist()
+        elif t == "outcome":
+            tmp = row[incolumns].values.tolist()
+            tmp[2] = np.nan
+            data_row = tmp
+            data_row.extend(row[outcolumns].values.tolist())
+            data_in_out.append(data_row)
+            data_row = []
+        # anyways, we need incolumns
+    prev, prevId = t, tid
 if len(data_row) != 0:
-  data_row.extend(df_join_sorted.iloc[-1][outcolumns].values.tolist())
-  data_in_out.append(data_row)
+    data_row.extend(df_join_sorted.iloc[-1][outcolumns].values.tolist())
+    data_in_out.append(data_row)
 
-df_in_out = pd.DataFrame(data_in_out, columns=['animal_id', 'name', 'intake_datetime', 'month_year', 'found_location',
-        'intake_type', 'intake_condition', 'animal_type', 'sex_upon_intake',
-        'age_upon_intake', 'breed', 'color','outcome_datetime', 'date_of_birth', 'outcome_type',
-       'outcome_subtype', 'sex_upon_outcome', 'age_upon_outcome'])
+df_in_out = pd.DataFrame(data_in_out, columns=['animal_id', 'name', 'intake_datetime', 'found_location',
+                                               'intake_type', 'intake_condition', 'animal_type', 'sex_upon_intake',
+                                               'age_upon_intake', 'breed', 'color', 'outcome_datetime', 'date_of_birth',
+                                               'outcome_type',
+                                               'outcome_subtype', 'sex_upon_outcome', 'age_upon_outcome'])
 
-df_in_out.to_csv("../data/Austin_Animal_Center_Intakes_Outcomes.csv", index=False)
-
+df_in_out.to_csv("data/Austin_Animal_Center_Joined.csv", index=False)
